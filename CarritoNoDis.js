88a175188be8ca1f773605f635dab7b39f834dd6
PatLadino67 <pa.menares@duocuc.cl>
2024-04-12 20:24:03 -0400

    alerta carrito boton de productos

---
+++ CarritoNoDis.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const popoverButton = document.querySelector(".btonn");
+
+    var popover = new bootstrap.Popover(popoverButton, {
+        title: 'Carrito de compra',
+        content: "¡ El carrito no esta disponible de momento !",
+        trigger: "hover",
+        container: 'body'
+    });
+
+    popoverButton.addEventListener('click', function () {
+        popover.toggle();
+    });
+})
