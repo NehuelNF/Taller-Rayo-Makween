57a07eb13283ce7e0fbe686c8cf447167561f570
PatLadino67 <pa.menares@duocuc.cl>
2024-05-12 23:14:21 -0400

    confirmacion en carro.js

---
+++ carro.js
@@ -0,0 +1,8 @@
+function confirmaciones(){
+    var respuesta = confirm("¿Está seguro de la información otorgada?");
+    if (respuesta == true){
+        return true;
+    } else {
+    return false;
+    }
+}
