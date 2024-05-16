54ba1ea3b9db283a36cce53937c96d55097682ec
NehuelNF <alfaronelson748@gmail.com>
2024-05-16 18:11:10 -0400

    API hora santiago abierto/cerrado

--- PaginaPrincipal.js
+++ PaginaPrincipal.js
@@ -13,3 +13,30 @@ document.addEventListener("DOMContentLoaded", function() {
         popover.toggle();
     });
 })
+
+//API DE HORA SANTIAGO
+$(function() {
+    // Función para obtener la hora actual de Santiago
+    function obtenerHoraSantiago() {
+        // Hacer la solicitud GET a la API de World Time para obtener la hora de Santiago
+        $.get("http://worldtimeapi.org/api/timezone/America/Santiago", function(data) {
+            var horaSantiago = new Date(data.utc_datetime); 
+            verificarHorario(horaSantiago);
+        });
+    }
+
+    // Función para verificar si el taller está abierto o cerrado
+    function verificarHorario(horaSantiago) {
+        var hora = horaSantiago.getHours();
+        
+        // Verificar si la hora está entre las 7:00 AM y las 7:00 PM
+        if (hora >= 7 && hora < 19) {
+            $("#estadoTaller").text("ABIERTO").removeClass("cerrado").addClass("abierto");;
+        } else {
+            $("#estadoTaller").text("CERRADO").removeClass("abierto").addClass("cerrado");
+        }
+    }
+
+    // Llamar a la función para obtener la hora de Santiago al cargar la página
+    obtenerHoraSantiago();
+});
