68e7401d624c80cd70ff23a876b3db8d13ed4bc2
NehuelNF <alfaronelson748@gmail.com>
2024-05-17 08:41:53 -0400

    Agregan validacion de carro

--- carro.js
+++ carro.js
@@ -1,8 +1,27 @@
-function confirmaciones(){
-    var respuesta = confirm("¿Está seguro de la información otorgada?");
-    if (respuesta == true){
-        return true;
+function validarCarro(){
+
+    var calles = document.getElementById("calles").value;
+    var numeros = document.getElementById("numeros").value;
+    var correo = document.getElementById("correo").value;
+    var contacto = document.getElementById("contacto").value;
+    var servicio = document.getElementById("Servicio").value;
+    var indicaciones = document.getElementById("indicaciones").value;
+
+    if (calles == ""){
+        alert('Ingrese la calle')
+    } else if (numeros == ""){
+        alert('Ingrese el numero')
+    } else if (correo == ""){
+        alert ('Ingrese el correo')
+    } else if (contacto == ""){
+        alert ('Ingrese un contacto')
+    } else if (servicio == "") {
+        alert ('Ingrese un servicio')
+    } else if (indicaciones == ""){
+        alert('Ingrese las indicaciones')
     } else {
-    return false;
+        confirm('¿Está seguro de la información otorgada?')
     }
+
+
 }
